2fba4ec57e85b0f498d20aa347c31e22cc79b3bf
unknown <bergador@posteo.net>
2017-10-09 23:39:23 +0200

    initial commit

---
+++ KTMemeAddon.lua
@@ -0,0 +1,26 @@
+function GetUnitName(unit, showServerName)
+	local name, server = UnitName(unit);
+	if ( server ) then
+		if ( showServerName ) then
+			return name.." - "..server;
+		else
+			return name..FOREIGN_SERVER_LABEL;
+		end
+	else
+		if name == "Kel'Thuzad" then
+			return "cold one"
+		elseif name == "Soldier of the Frozen Wastes" then
+			return "b o n y b o y e"
+		elseif name == "Soul Weaver" then
+			return "exploding girl"
+		elseif name == "Unstoppable Abomination" then
+			return "meaty boy"
+		elseif name == "Bergador" then
+			return "Big Dick Dwarf"
+		elseif name == "Guardian of Icecrown" then
+			return "spikyboy"
+		else
+			return name;
+		end
+	end
+end
