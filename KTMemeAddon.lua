fa98bdfe1c11ed6549d3edc1e8d2bf30f821af1e
unknown <bergador@posteo.net>
2017-10-10 18:56:57 +0200

    now overwrites UnitName()

--- KTMemeAddon.lua
+++ KTMemeAddon.lua
@@ -1,27 +1,22 @@
-function GetUnitName(unit, showServerName)
-	local name, server = UnitName(unit);
-	if ( server ) then
-		if ( showServerName ) then
-			return name.." - "..server;
-		else
-			return name..FOREIGN_SERVER_LABEL;
-		end
+CachedUnitName = UnitName
+
+function UnitName(unit)
+	local name = CachedUnitName(unit);
+	
+	if name == "Kel'Thuzad" then
+		return "cold one"
+	elseif name == "Soldier of the Frozen Wastes" then
+		return "b o n y b o y e"
+	elseif name == "Soul Weaver" then
+		return "explody girl"
+	elseif name == "Unstoppable Abomination" then
+		-- return "meaty boy"
+		return "Shield Wrath Male Night Elf Warrior Group 1"
+	elseif name == "Bergador" then
+		return "Big Dick Dwarf"
+	elseif name == "Guardian of Icecrown" then
+		return "spikyboy"
 	else
-		if name == "Kel'Thuzad" then
-			return "cold one"
-		elseif name == "Soldier of the Frozen Wastes" then
-			return "b o n y b o y e"
-		elseif name == "Soul Weaver" then
-			return "explody girl"
-		elseif name == "Unstoppable Abomination" then
-			-- return "meaty boy"
-			return "Shield Wrath Male Night Elf Warrior Group 1"
-		elseif name == "Bergador" then
-			return "Big Dick Dwarf"
-		elseif name == "Guardian of Icecrown" then
-			return "spikyboy"
-		else
-			return name;
-		end
+		return name;
 	end
 end
